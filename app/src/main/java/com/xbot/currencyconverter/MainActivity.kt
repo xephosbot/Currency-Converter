5a8f9306a6f6c5cad946e4dd4be125095b087904
xephosbot <xephosbot@gmail.com>
2024-07-21 00:28:59 +0500

    Initial commit

--- app/src/main/java/com/xbot/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/xbot/currencyconverter/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.xbot.currencyconverter.ui.feature.CurrencyConverterApp
+import com.xbot.currencyconverter.ui.CurrencyConverterApp
 import com.xbot.currencyconverter.ui.theme.CurrencyConverterTheme
 import dagger.hilt.android.AndroidEntryPoint
 
---
+++ app/src/main/java/com/xbot/currencyconverter/ui/CurrencyConverterApp.kt
@@ -0,0 +1,9 @@
+package com.xbot.currencyconverter.ui
+
+import androidx.compose.runtime.Composable
+import com.xbot.currencyconverter.navigation.CurrencyConverterNavHost
+
+@Composable
+fun CurrencyConverterApp() {
+    CurrencyConverterNavHost()
+}
